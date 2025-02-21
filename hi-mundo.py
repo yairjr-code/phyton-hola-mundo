8f3a2b632c3094f3d3af54b3f3a3a13bd07f032f
yairjr-code <eduardojad1102@gmail.com>
2025-02-21 10:39:41 -0500

    Inicio de todo programador, en phyton Hola mundo

---
+++ hi-mundo.py
@@ -0,0 +1,14 @@
+#Inicio de todo programador, en phyton "Hola mundo"
+
+print ("Hola Mundo")
+
+#configurar github pro primera vez
+#
+
+## 1. git config --global user.name "yairjr-code"
+#  2. git config --global user.mail "eduardojad1102@gmail.com"
+
+ #reposcitorio local se inicia
+ #git init
+ #git add .\Hola-mundo.py
+ #git commit -m "Inicio de todo programador, en phyton "Hola mundo"""
